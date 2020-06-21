fcc27e316244b37882a498197d6274ace4cf5a61
Aurnab Islam <aurnabislam@gmail.com>
2020-06-21 09:53:10 -0400

    first commit

---
+++ server.js
@@ -0,0 +1,35 @@
+const express = require("express");
+var exphbs  = require('express-handlebars');
+
+const app = express();
+
+
+app.engine('handlebars', exphbs());
+app.set('view engine', 'handlebars');
+
+app.use(express.static("public"))
+
+
+app.get("/",(req,res)=>{
+
+    res.render("index",{
+       title : "Home Page" 
+    })
+
+});
+ 
+app.get("/productListings",(req,res)=>{
+    res.render("index",{
+        title : "product listing page e"
+    })
+
+});
+
+
+const PORT = 3000;
+
+app.listen(port, () => {
+     
+    console.log('up n running');
+       
+})
